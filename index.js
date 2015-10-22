a3a59227509a1337d98b3fdac74e15a202b184cf
Bradley Booth <bradley.booth@gmail.com>
2015-10-21 22:53:52 -0700

    fix css path

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ module.exports = {
     included: function( app ) {
         this._super.included( app );
         app.import('bower_components/bootstrap-tour/build/js/bootstrap-tour-standalone.js');
-        app.import('bower_components/bootstrap-tour/build/bootstrap-tour.css');
+        app.import('bower_components/bootstrap-tour/build/css/bootstrap-tour.css');
     }
 };
